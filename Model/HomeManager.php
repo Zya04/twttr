80ab32832341de42d8a48d89d7c467291f1867e7
Zya Nassurally <zya1999@hotmail.fr>
2018-05-05 23:53:28 +0200

    added link database for follow

--- Model/HomeManager.php
+++ Model/HomeManager.php
@@ -34,4 +34,33 @@ class HomeManager
         return $data;
     
     }
+
+    public function follow()
+    {
+        $dbm = DBManager::getInstance();
+        $pdo = $dbm->getPdo();
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $link = mysqli_connect("localhost", "root", "", "twttr");
+
+        $getFollowId = intval($_GET['followedid']);
+
+        if($getFollowId != $_SESSION['id'])
+        {
+            $alreadyFollowed = $pdo->prepare('SELECT * FROM follow WHERE id_followed = ? AND id_following = ?');
+            $alreadyFollowed->execute(array($_SESSION['user_id'], getFollowId));
+            $alreadyFollowed = $alreadyFollowed->rowCount();
+
+            if($alreadyFollowed == 0)
+            {
+                $addFollow = $pdo->prepare('INSERT INTO follow(id_follower, id_following) VALUES (?,?)');
+                $addFollow->execute(array($_SESSION['user_id'], getFollowId));
+
+            } elseif ($alreadyFollowed == 1) 
+            {
+                $deleteFollow = $pdo->prepare('DELETE FROM follow WHERE id_follower = ? AND id_following = ?');
+                $deleteFollow->execute(array($_SESSION['user_id'], getFollowId));
+            }
+        }
+    }
 }
